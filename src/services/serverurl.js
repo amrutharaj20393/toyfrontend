6ce2a3c9a2ee91829a833c2e5c541fe68d9779ee
Amrutha Raj <144985452+amrutharaj20393@users.noreply.github.com>
2025-04-21 23:13:06 +0530

    Update serverurl.js

--- src/services/serverurl.js
+++ src/services/serverurl.js
@@ -1,1 +1,1 @@
-export const serverurl='https://toystore-9ul5.onrender.com'git remote add origin https://github.com/amrutharaj20393/ToyFrontendNew.git
+export const serverurl='https://toystore-9ul5.onrender.com'
